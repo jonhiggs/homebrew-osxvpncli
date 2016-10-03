0c30f7751e2eeaf84f5a3cdcb582db5ce649a58f
Jon Higgs <jhiggs@eml.cc>
2016-10-03 20:50:10 +1100

    Add formula

---
+++ Formula/osxvpncli.rb
@@ -0,0 +1,16 @@
+class Osxvpncli < Formula
+  desc "A wrapper to help you start and stop your VPN connections"
+  homepage "https://github.com/jonhiggs/osxvpncli"
+  url "https://github.com/jonhiggs/osxvpncli/archive/0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "4ec7fe32565866046613dbe3cd6359a5bc34cb19f97e775d3bf839d5fe9568cf"
+
+  def install
+    system "mv vpn osxvpncli"
+    bin.install 'osxvpncli'
+  end
+
+  test do
+    system "true"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-osxvpncli
+
+The Homebrew formula to install [osxvpncli](https://github.com/jonhiggs/osxvpncli).
+
+## Usage
+
+```ShellSession
+    $ brew tap jhiggs/homebrew-osxvpncli
+    $ brew install osxvpncli
+```
