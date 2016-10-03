94645ad7cb0639880703179c0e925cfbec0490ee
Jon Higgs <jhiggs@eml.cc>
2016-10-03 21:15:17 +1100

    Update to 0.2.2

--- Formula/osxvpncli.rb
+++ Formula/osxvpncli.rb
@@ -1,13 +1,11 @@
 class Osxvpncli < Formula
   desc "A wrapper to help you start and stop your VPN connections"
   homepage "https://github.com/jonhiggs/osxvpncli"
-  url "https://github.com/jonhiggs/osxvpncli/archive/0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "4ec7fe32565866046613dbe3cd6359a5bc34cb19f97e775d3bf839d5fe9568cf"
+  url "https://github.com/jonhiggs/osxvpncli/archive/0.2.2.tar.gz"
+  version "0.2.2"
+  sha256 "e34720149dabf45f8cb4b29740133bd8df9744028a76c26367e1fffd52a78a4d"
 
   def install
-    system "mv vpn osxvpncli"
-    system "mv bash_completion.d/vpn bash_completion.d/osxvpncli"
     bash_completion.install Dir["bash_completion.d/osxvpncli"]
     bin.install 'osxvpncli'
   end
